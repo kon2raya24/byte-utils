f21a1f530fdcbe4ff35b11104e56a9baf527c709
kon2raya24 <konturaya@gmail.com>
2026-06-27 09:01:58 +0000

    docs: add JSDoc comments to formatbytes.ts

--- src/formatbytes.ts
+++ src/formatbytes.ts
@@ -1,3 +1,8 @@
+/**
+ * formatBytes
+ * @param bytes
+ * @param decimals = 2
+ */
 export function formatBytes(bytes: number, decimals = 2): string {
   if (bytes === 0) return "0 B";
   const k = 1024;
